ce968a3fe837f05789e6c02ac699b73b1d12966d
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 11:17:41 -0700

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -8,3 +8,24 @@ def twoSum(nums, target):
 Time Complexity: O(n^2) because of the for loop and the "in" operator
 Space Complexity: O(1) no extra space being used
 """
+
+def two_sum_optimized(nums, target):
+
+    elements = {}
+
+    for i in range(len(nums)):
+        if nums[i] not in elements:
+            elements[nums[i]] = [i,1]  # store element in dictionary with its index
+        elements[nums[i]][-1] += 1     # increment times appears
+
+        temp = target-nums[i]
+        vals = elements.get(temp)
+
+        #if compliment num in dict and index is not the same and times it appears is not zero
+        if temp in elements and vals[0] != i and vals[-1] > 0:
+            return sorted([i, vals[0]])
+        
+"""
+Time Complexity: O(n) because of the for loop
+Space Complexity: O(n) because of dictionary storing values
+"""      
