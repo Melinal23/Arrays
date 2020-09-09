a19039a1fbd441ce2c96458046f0f3869a8abf6c
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 13:48:34 -0700

    Update two_sum_III.py

--- two_sum_III.py
+++ two_sum_III.py
@@ -1,25 +1,25 @@
 class TwoSum:
-		def __init__(self):
-				data_structure = []
+	def __init__(self):
+		data_structure = []
 				
-		# function that adds the number "input" to an internal data structure
-		def add(self, input):
-				self.data_structure.append(input)
+	# function that adds the number "input" to an internal data structure
+	def add(self, input):
+		self.data_structure.append(input)
 						
-		# function to find if there exists any pair of numbers which sum is equal to "value"
-    def find(self, value):
-						pairs = {}
-						
-						vals = self.data_structure
-						
-						for i in range(len(vals)):
-										if vals[i] not in pairs:
-														pairs[vals[i]] = [i, 1]
-										else:
-														pairs[vals[i]][-1] += 1 
+	# function to find if there exists any pair of numbers which sum is equal to "value"
+    	def find(self, value):
+		pairs = {}
 
-										comp = value - vals[i]
-										info = pairs.get(comp)
+		vals = self.data_structure
 
-										if info and info[0] != i and info[1] > 1:
-														return [i, info[0]]									
+		for i in range(len(vals)):
+			if vals[i] not in pairs:
+				pairs[vals[i]] = [i, 1]
+			else:
+				pairs[vals[i]][-1] += 1 
+
+			comp = value - vals[i]
+			info = pairs.get(comp)
+
+			if info and info[0] != i and info[1] > 1:
+				return [i, info[0]]									
